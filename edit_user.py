3ff1e6c30d16ab266a0298c74195e17cb2c38dc0
jtangqt <ajasmineflower@gmail.com>
2018-04-08 22:47:00 -0400

    changed db and also created insert user function

---
+++ edit_user.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import MySQLdb
+import datetime
+
+#opening db connection
+db = MySQLdb.connect("localhost", "root", "password", "playgroundapr8")
+
+#preparing cursor option using cursor() method
+cursor = db.cursor()
+
+class user:
+	def __init__(self, user_id, f_name, l_name, u_name, pwrd, email, time):
+		self.user_id = user_id
+		self.f_name = f_name
+		self.l_name = l_name
+		self.u_name = u_name
+		self.pwrd = pwrd
+		self.email = email
+		self.time = time
+
+
+#insert new user
+def insert_u(user):
+	ins = ("INSERT INTO User" 
+			"(user_id, first_name, last_name, username, password, email, signup_date)"
+			"VALUES (%s, %s, %s, %s, %s, %s, %s)")
+			
+
+	data_user = (user.user_id, user.f_name, user.l_name, user.u_name, user.pwrd, user.email, user.time)
+
+	cursor.execute(ins, data_user)
+	
+
+
+#read user info
+def read_u(f_name, l_name, u_name): 
+	# if u_name:
+	# 	read = ("SELECT * FROM User WHERE username = %s")
+	# 	value = u_name
+	# else:
+	# 	read = ("SELECT * FROM User WHERE first_name = %s AND last_name = %s")	
+	# 	value = (f_name, l_name)
+
+	read = ("SELECT * FROM User WHERE first_name = %s AND last_name = %s")	
+	value = (f_name, l_name)
+	for user in cursor.execute(read, value): 
+		if user.with_rows():
+			user.fetchall()
+		else: 
+			print("%s %s's password is %s".format(first_name, last_name, password))
+
+#update user
+def update_u():
+	print("hello")
+
+#delete user
+def delete_u():
+	print("hello")
+
+user_id = cursor.lastrowid
+start_time = datetime.datetime.now()
+me = user(user_id, 'Jasmine', 'Tang', 'jtangqt', 'hehexd', 'tang@cooper.edu', start_time)
+			
+#insert_u(me)
+#read_u('Jasmine', 'Tang', 0)
+
+
+# cursor.execute(ins, data_user)
+db.commit()
+
+
+cursor.close()
+db.close()
