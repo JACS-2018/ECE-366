4d5d6ffbecae05477b0e8d710896e1236cb1138b
Cwei1 <cardylw@gmail.com>
2018-05-01 15:04:24 -0400

    OK FINAL PROJECT VERSION BUGS FIXED

--- friend/friend.controller.js
+++ friend/friend.controller.js
@@ -28,9 +28,6 @@
                 .then(function (user) {
                     vm.user = user['person'][0];
                     var getpost = JSON.stringify({'user_id_a':user['person'][0].username,'user_id_b':$location.url().split('#')[1]});
-                    loadPosts(getpost);
-                    requestexists(user['person'][0].username);
-                    requestexists2(user['person'][0].username);
                 });
         }
 
