1232bf1c08d5d151fba94841e32fe3794c2a54eb
Cwei1 <cardylw@gmail.com>
2018-05-02 15:24:32 -0400

    Added Search Functionality PROJECT COMPLETE

--- friend/friend.controller.js
+++ friend/friend.controller.js
@@ -13,6 +13,11 @@
         vm.allUsers = [];
         vm.deleteUser = deleteUser;
         vm.allUsersLen = 0;
+        vm.find = find;
+        vm.searching = 0;
+        vm.person = null;
+        vm.exists = 0;
+        vm.find2 = find2;
 
         initController();
 
@@ -29,6 +34,35 @@
                     vm.user = user['person'][0];
                     var getpost = JSON.stringify({'user_id_a':user['person'][0].username,'user_id_b':$location.url().split('#')[1]});
                 });
+        }
+
+        function find(username) {
+             if (typeof username !== 'undefined'){
+                if(username.length !== 0){
+                    vm.searching = 1;
+                    UserService.GetByUsername(username)
+                        .then(function (user) {
+                            vm.person = user['person'];
+                            if (vm.person.length !== 0){
+                                vm.exists = 1;
+                            }
+                            else{
+                                vm.exists = 0;
+                            }
+                        });
+                }
+                else{
+                    vm.searching = 0;
+                }
+            }
+            else{
+                vm.searching = 0;
+            }
+            
+        }
+
+        function find2(){
+            vm.searching = 0;
         }
 
         function loadAllUsers() {
